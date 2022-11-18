5444402342e251925c4711fd27719bb731165817
xsy <1131585528@qq.com>
2022-11-18 23:15:45 +0800

    11/18 23:15

--- ax_mat.py
+++ ax_mat.py
@@ -18,14 +18,21 @@ CANVAS_CENTER_Y=CANVAS_HEIGHT/2
 #IMAGE_ENLARGE=11
 HEART_COLOR='#FFFFFF'
 
+
+def calc(time):
+    t=np.linspace(0,t,200)
+    return ( 16*(np.sin(t)**3),(13*np.cos(t)-5*np.cos(2*t)-2*np.cos(3*t)-np.cos(4*t)))
+
 if __name__=='__main__':
     fig=plt.figure(figsize=(CANVAS_WIDTH*2,CANVAS_HEIGHT*2))
     ax=fig.add_subplot()
     ax.set_title("Heart",fontdict={"fontsize":18})
-    t=np.linspace(0,10,200)
+    line,=ax.plot([],[],lw=2)
     
-    x=16*(np.sin(t)**3)
-    y=(13*np.cos(t)-5*np.cos(2*t)-2*np.cos(3*t)-np.cos(4*t))
+    
+    
+   
+    
     
     ax.plot(x,y)
     plt.show()
